c064c0d758e67ccf9ad45626a9de70fa2593338d
evatry <shanghunhun@gmail.com>
2022-10-17 15:44:06 +0800

    add a file

--- function_test1.py
+++ function_test1.py
@@ -1,19 +1,19 @@
-form selenium import webdriver
-import umittest
+from selenium import webdriver
+import unittest
 
-class NewivistorTest(unittest.TestCase):
-
-    def setup(self):
-        self.browser = webdriver.Firfox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        #self.browser.implicity_wait(1)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start(self):
+    def test_start_a_list(self):
         self.browser.get('http://localhost:8000')
-
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
-    unittest.main(warning = 'ignore')
+    unittest.main(warnings='ignore')
+

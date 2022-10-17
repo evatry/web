e358e7e4aba4ee24584f2624a7bdf1b22dcc1680
evatry <shanghunhun@gmail.com>
2022-10-17 15:23:10 +0800

    add new file

--- function_test.py
+++ function_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+assert 'To-do' in browser.title
+bowser.quit()
+

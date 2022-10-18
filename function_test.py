c330ae5d3dde53ac8cb348e5a32c11e14d085154
evatry <shanghunhun@gmail.com>
2022-10-18 15:33:47 +0800

    change

--- function_test.py
+++ function_test.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicity_wait(3)
+        #self.browser.implicity_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser,find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.asserIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -33,6 +33,9 @@ class NewVisitorTest(unittest.TestCase):
                 any(row.text == '1: Buy peacock frathers' for row in rows)
                 )
         self.fail('Finish the test!')
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
 
 
 
